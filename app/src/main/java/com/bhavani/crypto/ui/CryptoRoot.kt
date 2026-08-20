2e4997f693c24b079d1bb6d44257749ed2a32f3a
Sagar Parmar <seaparmar01@gmail.com>
2026-08-20 21:24:34 +0530

    fix: import Compose Stroke drawscope

--- app/src/main/java/com/bhavani/crypto/ui/CryptoRoot.kt
+++ app/src/main/java/com/bhavani/crypto/ui/CryptoRoot.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
